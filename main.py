2ac87a81dda1b6ea55f646dfa348978d1593acf2
Dskimz <danskimin@eti360.com>
2025-02-11 08:27:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from knack_services import process_all_locations
+from knack_services import process_unprocessed_locations
 
 if __name__ == "__main__":
-    result = process_all_locations()
+    result = process_unprocessed_locations()
     print(result)
+
